8b9b3026dc7c805c372dcf192a4fcd64110352eb
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2025-06-29 15:39:11 -0600

    fix home-manager version to 25.05 to match Nixpkgs

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   # (the Flakes -> NixOS module section)
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 

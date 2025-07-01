b432765f87ec6ec090773a9d9b1565d23f17e007
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2025-07-01 08:48:39 -0600

    add readme, and stylix for theming

---
+++ README.md
@@ -0,0 +1,7 @@
+# NixOS Configuration
+
+## Resources
+### Theming
+[Rose Pine Kitty](https://github.com/rose-pine/kitty)
+[Rose Pine Rofi](https://github.com/rose-pine/rofi)
+
--- flake.nix
+++ flake.nix
@@ -6,17 +6,26 @@
   # (the Flakes -> NixOS module section)
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    # Home manager config (using property assignment example)
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Stylix for system theming (using object config example)
+    stylix = {
+      url = "github:danth/stylix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, stylix, ... }: {
     nixosConfigurations = {
       # Example shows `hostname` but I'm using the default `sudo nix-rebuild switch` command
       # which appears to use the default `nixos` from the install
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+	  stylix.nixosModules.stylix
           ./configuration.nix
 	  home-manager.nixosModules.home-manager
 	  {
